45b8512dafb5329af551f9bb468e2f9e9d405b22
Manar <162815851+manom8@users.noreply.github.com>
2025-05-08 13:35:56 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import FallingPiece from './FallingPiece';
 import { Box } from '@mui/material';
 import { motion } from "framer-motion";
 import TypingText from './TypingText';
 import Bye from './Bye';
 
 function App() {
-  const [pieces, setPieces] = useState([]);
   const [phase, setPhase] = useState("intro");
   const [showButtons, setShowButtons] = useState(false);
   const [showBye, setShowBye] = useState(false);
   const [hasChosenBefore, setHasChosenBefore] = useState(false);
-  const MAX_PIECES = 10;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPieces(prev => {
-        if (prev.length >= MAX_PIECES) return prev;
-        const id = Date.now() + Math.random();
-        const width = Math.random() * 20 + 10;
-        const height = Math.random() * 30 + 20;
-        const xPos = Math.random() * window.innerWidth;
-        const rotateStart = Math.random() * 360;
-        const rotateEnd = rotateStart + (Math.random() * 360 - 180);
-        const duration = Math.random() * 5 + 8;
-        const color = Math.random() < 0.5 ? '#fff' : '#f33';
-        return [...prev, { id, width, height, xPos, rotateStart, rotateEnd, duration, color }];
-      });
-    }, 900);
-    return () => clearInterval(interval);
-  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -48,8 +27,6 @@ function App() {
     }
   }, []);
 
-  const handleRemove = id => setPieces(prev => prev.filter(p => p.id !== id));
-
   const handleChoice = (type) => {
     localStorage.setItem("userChoice", type);
     setShowBye(true);
@@ -57,10 +34,6 @@ function App() {
 
   return (
     <div className="App">
-      {pieces.map(p => (
-        <FallingPiece key={p.id} {...p} onComplete={() => handleRemove(p.id)} />
-      ))}
-
       <Box sx={{ position: "relative", width: "100vw", height: "100vh" }}>
         <motion.img
           src="/logo3.png"
